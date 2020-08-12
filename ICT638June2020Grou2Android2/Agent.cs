d0fca599e8ad25cf5ff8646077e0330850dd61be
Administrator <Administrator@DESKTOP-EU336QI>
2020-08-12 19:08:35 +1200

    agent class

---
+++ ICT638June2020Grou2Android2/Agent.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace ICT638June2020Grou2Android2
+{
+    public class Agent
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phonenumber { get; set; }
+    }
+}
