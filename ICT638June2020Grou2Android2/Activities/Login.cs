126f9c8caf3c31ad2b523e9f390879795bdc012b
Kunye Song <1811868@nzse.ac.nz>
2020-08-12 15:55:16 +1200

    User cs and Login act

--- ICT638June2020Grou2Android2/Activities/Login.cs
+++ ICT638June2020Grou2Android2/Activities/Login.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 
 using Android.App;
@@ -17,9 +19,41 @@ namespace ICT638June2020Grou2Android2.Activities
     {
         protected override void OnCreate(Bundle savedInstanceState)
         {
+            SetContentView(Resource.Layout.login_layout);
             base.OnCreate(savedInstanceState);
+            Button btnLogin = FindViewById<Button>(Resource.Id.btnLoginUser);
+            btnLogin.Click += BtnLogin_Click;
 
-            // Create your application here
+        }
+
+        private void BtnLogin_Click(object sender, EventArgs e)
+        {
+            EditText eduseraccount = FindViewById<EditText>(Resource.Id.edUserAccount);
+            EditText eduserpassword = FindViewById<EditText>(Resource.Id.edUserPassword);
+
+            string url = "https://10.0.2.2:5001/api/Registers/1";
+            string login77 = "";
+            var httpWebRequest = new HttpWebRequest(new Uri(url));
+            httpWebRequest.ServerCertificateValidationCallback = delegate { return true; };
+            httpWebRequest.ContentType = "application/json";
+            httpWebRequest.Method = "Get";
+
+
+
+            HttpWebResponse response = (HttpWebResponse)httpWebRequest.GetResponse();
+            using (StreamReader reader = new StreamReader(response.GetResponseStream(), Encoding.UTF8))
+            {
+                login77 = reader.ReadToEnd();
+            }
+
+
+            User user = new User();
+            user = Newtonsoft.Json.JsonConvert.DeserializeObject<User>(login77);
+
+            if (eduseraccount.Text == user.uniquename && eduserpassword.Text == user.password)
+            {
+                StartActivity(typeof(View));
+            }
         }
     }
 }
