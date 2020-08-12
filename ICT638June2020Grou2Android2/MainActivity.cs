f7d77471b43817b72fa580c04096b2dd1584e5d9
Kunye Song <1811868@nzse.ac.nz>
2020-08-12 15:49:36 +1200

    Main act

--- ICT638June2020Grou2Android2/MainActivity.cs
+++ ICT638June2020Grou2Android2/MainActivity.cs
@@ -3,6 +3,7 @@ using Android.OS;
 using Android.Support.V7.App;
 using Android.Runtime;
 using Android.Widget;
+using ICT638June2020Grou2Android2.Activities;
 
 namespace ICT638June2020Grou2Android2
 {
@@ -15,7 +16,16 @@ namespace ICT638June2020Grou2Android2
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
+
+            Button btn_Str = FindViewById<Button>(Resource.Id.btn_start);
+            btn_Str.Click += Btn_Str_Click;
         }
+
+        private void Btn_Str_Click(object sender, System.EventArgs e)
+        {
+            StartActivity(typeof(Login));
+        }
+
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
