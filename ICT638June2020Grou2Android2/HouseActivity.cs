d8197e400ce29815f0c0cfb0733666089976dc3b
Peizhen Lao <1812382@nzse.ac.nz>
2020-08-12 17:51:07 +1200

    Room Recycler View

---
+++ ICT638June2020Grou2Android2/HouseActivity.cs
@@ -0,0 +1,269 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.Gms.Maps;
+using Android.Gms.Maps.Model;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Xamarin.Essentials;
+
+namespace ICT638June2020Grou2Android2
+{
+    [Activity(Label = "HouseActivity")]
+    public class HouseActivity : Activity, IOnMapReadyCallback
+    {
+        private TextView weekofrent, Bedroom, Bathroom, AssignedAgentid;
+        int id;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            SetContentView(Resource.Layout.RoomDetail);
+
+            base.OnCreate(savedInstanceState);
+
+            id = Intent.GetIntExtra("id", 1);
+            weekofrent = FindViewById<TextView>(Resource.Id.tvRoom1);
+            Bedroom = FindViewById<TextView>(Resource.Id.tvBedroom1);
+            Bathroom = FindViewById<TextView>(Resource.Id.tvBathroom1);
+            AssignedAgentid = FindViewById<TextView>(Resource.Id.Agentid1);
+            Button btnbacktomenu = FindViewById<Button>(Resource.Id.btnbacktomenu);
+            btnbacktomenu.Click += Btnbacktomenu_Click;
+
+            //share button
+            Button button1 = FindViewById<Button>(Resource.Id.button1);
+            button1.Click += Button1_Click;
+            Button button2 = FindViewById<Button>(Resource.Id.button2);
+            button2.Click += Button2_Click;
+
+
+            string url = "https://10.0.2.2:5001/api/RoomInfoes/" + id;
+            string room1 = "";
+            var httpWebRequest = new HttpWebRequest(new Uri(url));
+            httpWebRequest.ServerCertificateValidationCallback = delegate { return true; };
+            httpWebRequest.ContentType = "application/json";
+            httpWebRequest.Method = "GET";
+
+            HttpWebResponse response = (HttpWebResponse)httpWebRequest.GetResponse();
+            using (StreamReader reader = new StreamReader(response.GetResponseStream(), Encoding.UTF8))
+            {
+                room1 = reader.ReadToEnd();
+            }
+
+            Room room = new Room();
+            room = Newtonsoft.Json.JsonConvert.DeserializeObject<Room>(room1);
+
+            weekofrent.Text = room.roomrent;
+            Bedroom.Text = room.NumOfBedroom.ToString();
+            Bathroom.Text = room.NumOfBathroom.ToString();
+            AssignedAgentid.Text = room.Agentid.ToString();
+            //map
+            var mapFrag = MapFragment.NewInstance();// mapOptions);
+
+            FragmentManager.BeginTransaction()
+                                    .Add(Resource.Id.mapFragContainer, mapFrag, "map_fragment")
+                                   .Commit();
+
+            mapFrag.GetMapAsync(this);
+
+
+        }
+        //share button
+        private async void Button2_Click(object sender, EventArgs e)
+        {
+            TextView textView1 = FindViewById<TextView>(Resource.Id.textView1);
+            string messageText = textView1.Text;
+            string recipient = "123";
+            try
+            {
+                var message = new SmsMessage(messageText, new[] { recipient });
+                await Sms.ComposeAsync(message);
+            }
+            catch (FeatureNotSupportedException ex)
+            {
+                // Sms is not supported on this device.
+            }
+            catch (Exception ex)
+            {
+                // Other error has occurred.
+            }
+        }
+
+        private async void Button1_Click(object sender, EventArgs e)
+        {
+
+            TextView textView1 = FindViewById<TextView>(Resource.Id.textView1);
+            string text = textView1.Text;
+            await Share.RequestAsync(new ShareTextRequest
+            {
+                Text = text,
+                Title = "Share Text"
+            });
+        }
+
+        private void Btnbacktomenu_Click(object sender, EventArgs e)
+        {
+            Intent startA = new Intent(this, typeof(MainActivity));
+            StartActivity(startA);
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+        //map
+        public void OnMapReady(GoogleMap googleMap)
+        {
+            googleMap.MapType = GoogleMap.MapTypeNormal;
+            googleMap.UiSettings.ZoomControlsEnabled = true;
+            googleMap.UiSettings.CompassEnabled = true;
+
+            getCurrentLoc(googleMap);
+        }
+
+        public async void getLastLocation(GoogleMap googleMap)
+        {
+            Console.WriteLine("Test - LastLoc");
+            try
+            {
+                var location = await Geolocation.GetLastKnownLocationAsync();
+                if (location != null)
+                {
+                    Console.WriteLine($"Last Loc - Latitude: {location.Latitude}, Longitude: {location.Longitude}, Altitude: {location.Altitude}");
+                    MarkerOptions curLoc = new MarkerOptions();
+                    curLoc.SetPosition(new LatLng(location.Latitude, location.Longitude));
+                    var address = await Geocoding.GetPlacemarksAsync(location.Latitude, location.Longitude);
+                    var placemark = address?.FirstOrDefault();
+                    var geocodeAddress = "";
+                    if (placemark != null)
+                    {
+                        geocodeAddress =
+                            $"AdminArea:       {placemark.AdminArea}\n" +
+                            $"CountryCode:     {placemark.CountryCode}\n" +
+                            $"CountryName:     {placemark.CountryName}\n" +
+                            $"FeatureName:     {placemark.FeatureName}\n" +
+                            $"Locality:        {placemark.Locality}\n" +
+                            $"PostalCode:      {placemark.PostalCode}\n" +
+                            $"SubAdminArea:    {placemark.SubAdminArea}\n" +
+                            $"SubLocality:     {placemark.SubLocality}\n" +
+                            $"SubThoroughfare: {placemark.SubThoroughfare}\n" +
+                            $"Thoroughfare:    {placemark.Thoroughfare}\n";
+
+                    }
+                    curLoc.SetTitle("You were here" + geocodeAddress);
+                    curLoc.SetIcon(BitmapDescriptorFactory.DefaultMarker(BitmapDescriptorFactory.HueAzure));
+                    googleMap.AddMarker(curLoc);
+                }
+            }
+            catch (FeatureNotSupportedException fnsEx)
+            {
+                // Handle not supported on device exception
+                Toast.MakeText(this, "Feature Not Supported", ToastLength.Short);
+            }
+            catch (FeatureNotEnabledException fneEx)
+            {
+                // Handle not enabled on device exception
+                Toast.MakeText(this, "Feature Not Enabled", ToastLength.Short);
+            }
+            catch (PermissionException pEx)
+            {
+                // Handle permission exception
+                Toast.MakeText(this, "Needs more permission", ToastLength.Short);
+            }
+            catch (Exception ex)
+            {
+                // Unable to get location
+                Toast.MakeText(this, "Unable to get location", ToastLength.Short);
+            }
+        }
+
+        public async void getCurrentLoc(GoogleMap googleMap)
+        {
+            Console.WriteLine("Test - CurrentLoc");
+            try
+            {
+                var request = new GeolocationRequest(GeolocationAccuracy.Best);
+                var location = await Geolocation.GetLocationAsync(request);
+
+                if (location != null)
+                {
+                    Console.WriteLine($"current Loc - Latitude: {location.Latitude}, Longitude: {location.Longitude}, Altitude: {location.Altitude}");
+                    MarkerOptions curLoc = new MarkerOptions();
+                    curLoc.SetPosition(new LatLng(location.Latitude, location.Longitude));
+
+
+                    var address = await Geocoding.GetPlacemarksAsync(location.Latitude, location.Longitude);
+                    var placemark = address?.FirstOrDefault();
+                    var geocodeAddress = "";
+                    if (placemark != null)
+                    {
+                        geocodeAddress =
+                            $"AdminArea:       {placemark.AdminArea}\n" +
+                            $"CountryCode:     {placemark.CountryCode}\n" +
+                            $"CountryName:     {placemark.CountryName}\n" +
+                            $"FeatureName:     {placemark.FeatureName}\n" +
+                            $"Locality:        {placemark.Locality}\n" +
+                            $"PostalCode:      {placemark.PostalCode}\n" +
+                            $"SubAdminArea:    {placemark.SubAdminArea}\n" +
+                            $"SubLocality:     {placemark.SubLocality}\n" +
+                            $"SubThoroughfare: {placemark.SubThoroughfare}\n" +
+                            $"Thoroughfare:    {placemark.Thoroughfare}\n";
+
+                    }
+
+
+                    curLoc.SetTitle("You are here");// + geocodeAddress);
+                    curLoc.SetIcon(BitmapDescriptorFactory.DefaultMarker(BitmapDescriptorFactory.HueAzure));
+
+                    googleMap.AddMarker(curLoc);
+
+
+                    CameraPosition.Builder builder = CameraPosition.InvokeBuilder();
+                    builder.Target(new LatLng(location.Latitude, location.Longitude));
+                    builder.Zoom(18);
+                    builder.Bearing(155);
+                    builder.Tilt(65);
+
+                    CameraPosition cameraPosition = builder.Build();
+
+                    CameraUpdate cameraUpdate = CameraUpdateFactory.NewCameraPosition(cameraPosition);
+
+                    googleMap.MoveCamera(cameraUpdate);
+                }
+                else
+                {
+                    getLastLocation(googleMap);
+                }
+            }
+            catch (FeatureNotSupportedException fnsEx)
+            {
+                // Handle not supported on device exception
+                Toast.MakeText(this, "Feature Not Supported", ToastLength.Short);
+            }
+            catch (FeatureNotEnabledException fneEx)
+            {
+                // Handle not enabled on device exception
+                Toast.MakeText(this, "Feature Not Enabled", ToastLength.Short);
+            }
+            catch (PermissionException pEx)
+            {
+                // Handle permission exception
+                Toast.MakeText(this, "Needs more permission", ToastLength.Short);
+            }
+            catch (Exception ex)
+            {
+                getLastLocation(googleMap);
+            }
+        }
+
+    }
+}
